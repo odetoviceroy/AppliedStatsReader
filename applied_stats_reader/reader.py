c9cd4cb76c52e2eb7a96c5390b1f592b48c02cbe
George Juarez <jjuarez@albany.edu>
2017-09-14 19:05:35 -0400

    Update reader.py

--- applied_stats_reader/reader.py
+++ applied_stats_reader/reader.py
@@ -57,6 +57,20 @@ def calc_beta_one():
 def calc_beta_zero():
 	return mean_y() - (calc_beta_one() * mean_x())
 
+def expected_y(ex):
+	return calc_beta_zero() + (calc_beta_one() * float(ex))
+
+def calc_sec():
+	if(len(x_vals) > 0):
+		return sum_sq_x() - (sum_x() * sum_x()/len(x_vals))
+	return -1
+
+def calc_sse():
+	if(len(x_vals) > 0):
+		sec = sum_sq_y() - ((sum_y() * sum_y()) / len(x_vals))
+		return sec - (calc_beta_one() * calc_beta_one()) * (calc_sec())
+	return -1		
+
 def print_all():
 	counter = 0
 	print "Row   X    Y"
@@ -64,23 +78,34 @@ def print_all():
 		print counter + 1, " ", ex, "  ", y_vals[counter]
 		counter += 1
 
-def expected_y(ex):
-	return calc_beta_zero() + (calc_beta_one() * float(ex))
+def print_help():
+	with open("help.txt") as f:
+		content = f.readlines()
+	for c in content:
+		print c,
 
 def main():
-	readFile(sys.argv[1])
-	if(sys.argv[2] == "print-table"):
-		print_all()
-	if(sys.argv[2] == "print-results"):
-		print "N: ", len(x_vals), "\nSum of X values: ", sum_x(), "\nSum of Y values: ", sum_y()
-		print "Sum of X times Y values: ", sum_x_times_y(), "\nSum of X^2 values:", sum_sq_x()
-		print "Mean X values: ", mean_x(), "\nMean Y values: ", mean_y()
-		print "Max X value: ", max(x_vals), "\nMin X value:", min(x_vals), "\nMax Y value: ", max(y_vals), "\nMin Y value: ", min(y_vals)
-		print "Beta[1]: ", calc_beta_one(), "\nBeta[0]: ", calc_beta_zero()
-		print "Regression Equation: Y =", calc_beta_zero(), "+", calc_beta_one(), "* X"
+	
+	if(len(sys.argv) == 2):
+		if(sys.argv[1] == "help"):
+			print_help()
 	if(len(sys.argv) > 2):
-		if(sys.argv[2].lower() == "x"):
-			print expected_y(sys.argv[3])
+		readFile(sys.argv[1])
+
+		if(sys.argv[2] == "print-table"):
+			print_all()
+		if(sys.argv[2] == "print-results"):
+			print "N: ", len(x_vals), "\nSum of X values: ", sum_x(), "\nSum of Y values: ", sum_y()
+			print "Sum of X times Y values: ", sum_x_times_y(), "\nSum of X^2 values:", sum_sq_x(), "\nSum of Y^2 values: ", sum_sq_y()
+			print "Mean X values: ", mean_x(), "\nMean Y values: ", mean_y()
+			print "Max X value: ", max(x_vals), "\nMin X value:", min(x_vals), "\nMax Y value: ", max(y_vals), "\nMin Y value: ", min(y_vals)
+			print "Beta[1]: ", calc_beta_one(), "\nBeta[0]: ", calc_beta_zero()
+			print "Regression Equation: Y =", calc_beta_zero(), "+", calc_beta_one(), "* X"
+			print "SSE: ", calc_sse()
+			print calc_sec()
+		if(len(sys.argv) > 2):
+			if(sys.argv[2].lower() == "x"):
+				print expected_y(sys.argv[3])
 
 if __name__  == "__main__":
 	main()
